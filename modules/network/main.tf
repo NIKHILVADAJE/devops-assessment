7b95d4ab0e155776b0c9943ebb3bd066eb71212d
Nikhil Vadaje <nikhilvadaje4548@gmail.com>
2026-07-07 20:29:01 +0530

    feat: complete infrastructure design with clean gitignore exclusions

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,8 +1,37 @@
-﻿variable "vpc_cidr" {}
+variable "vpc_cidr" {}
 variable "env" {}
-resource "aws_vpc" "main" { cidr_block = var.vpc_cidr; tags = { Name = "${var.env}-vpc" } }
-resource "aws_subnet" "public" { vpc_id = aws_vpc.main.id; cidr_block = "10.0.1.0/24" }
-resource "aws_subnet" "private" { vpc_id = aws_vpc.main.id; cidr_block = "10.0.2.0/24" }
-output "vpc_id" { value = aws_vpc.main.id }
-output "public_subnet_id" { value = aws_subnet.public.id }
-output "private_subnet_id" { value = aws_subnet.private.id }
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.env}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "${var.env}-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = "${var.env}-private-subnet"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private.id
+}
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,10 +1,15 @@
-﻿variable "vpc_id" {}
+variable "vpc_id" {}
 variable "private_subnet_id" {}
 variable "instance_class" {}
 variable "backup_retention_period" {}
 variable "deletion_protection" {}
 variable "env" {}
-resource "aws_security_group" "rds_sg" { name = "${var.env}-rds-sg"; vpc_id = var.vpc_id }
+
+resource "aws_security_group" "rds_sg" {
+  name   = "${var.env}-rds-sg"
+  vpc_id = var.vpc_id
+}
+
 resource "aws_db_instance" "db" {
   allocated_storage       = 20
   engine                  = "postgres"
